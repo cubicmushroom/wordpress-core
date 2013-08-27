ac7d0a4a3e41bac93c03c19c0e30c9ec309ba72d
Toby Griffiths <toby@cubicmushroom.co.uk>
2013-08-27 19:12:47 +0100

    Added code to allow providing of full $labels for post registration.

--- src/CubicMushroom/WP/Core/PostType.php
+++ src/CubicMushroom/WP/Core/PostType.php
@@ -72,7 +72,7 @@ if (!class_exists('CubicMushroom_WP_Core_PostType')) {
          * Labels for post type
          * @var array
          */
-        protected $labels;
+        static protected $labels;
 
         /**
          * Registers the custom post type
@@ -91,6 +91,9 @@ if (!class_exists('CubicMushroom_WP_Core_PostType')) {
         {
             $class = get_called_class();
             $args = wp_parse_args($class::$postArgs, self::$postArgs);
+            if (!empty($class::$labels)) {
+                $args['labels'] = $class::$labels;
+            }
             $postType = register_post_type($class::POST_SLUG, $args);
             if ($postType instanceof \WP_Error) {
                 throw new CubicMushroom_WP_Core_Exception_PostTypeRegistrationFailedException($class, $args, $postType);
